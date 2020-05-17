18f9d5af41ce5bf1748255c9014440f018326158
csoderberg <csoderbe@gmail.com>
2020-05-17 10:38:20 -0400

    add function to generate corr distritbutinos

--- indep_REmeta_analysis.R
+++ indep_REmeta_analysis.R
@@ -1,4 +1,20 @@
 #loading libraries
-library(mvtnorm)
+library(MASS)
 library(tidyverse)
 library(metafor)
+
+gen_corr_distribution <- function(corrs, sd_corrs, n_dvs){
+  
+  correlations <- c()  
+  
+    for (i in 1:length(corrs)) {
+      measure_correlation <- c(rnorm(n = n_dvs, mean = corrs[i], sd = sd_corrs), i)
+      correlations <- rbind(correlations, measure_correlation)
+    }
+  
+  correlations <- as_tibble(correlations) %>%
+                    rename(conserv_measure = n_dvs + 1) %>%
+                    pivot_longer(cols = 1:n_dvs, names_to = 'dv', values_to = 'dv_corr')
+  
+  return(correlations)
+}
